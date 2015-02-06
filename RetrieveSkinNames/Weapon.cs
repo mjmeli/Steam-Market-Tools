1be0748e1659f82521b83b4ecf057022e46eccb1
mjmeli <mjmeli@ncsu.edu>
2015-02-06 14:13:30 -0500

    Added weapon class

---
+++ RetrieveSkinNames/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveSkinNames
+{
+    class Weapon
+    {
+        private String name;
+        private List<String> skins;
+
+        /// <summary>
+        /// Weapon name
+        /// </summary>
+        public String Name { get; set; }
+        
+        /// <summary>
+        /// List of the names of skins for this weapon
+        /// </summary>
+        private List<String> Skins { get; set; }
+
+        /// <summary>
+        /// Create a new weapon object.
+        /// </summary>
+        /// <param name="name">Name of the weapon</param>
+        public Weapon(String name)
+        {
+            this.name = name;
+            this.skins = new List<String>();
+        }
+    }
+}
