a13a3bec4bbea78ce3fb71f31b39cfb80d18ed50
Derek Whatley <djwhatle@ncsu.edu>
2015-02-08 01:13:36 -0500

    normal session working, about to implement futures session

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.4
 import requests, getpass, sys
 from requests.auth import HTTPBasicAuth
+from requests_futures.sessions import FuturesSession
 
 #db connection
 dbHost = 'http://steamtrack:5984/'
@@ -28,8 +29,25 @@ if r.json().get('error') == 'unauthorized':
 
 #get json document containing all gun variations
 r = requests.get(dbHost + 'tracked_steam_item_names/weapons_list', auth=dbAuth)
-print(r.json())
+#print(str(r.json()).encode('utf-8').strip())
 
 #for each market item in the document, build a string
 #http://steamcommunity.com/market/priceoverview/?country=US&currency=1&appid=730&market_hash_name=M4A1-S%20%7C%20Guardian%20%28Minimal%20Wear%29
-#store the price data 
+#store the price data
+complete_weapon_name = None
+
+#flattened
+s = requests.Session()
+for weapon_base_name in r.json().get('weapons'):
+   
+   for weapon_skin_name in weapon_base_name.get('skins'):
+      for weapon_condition in weapon_skin_name.get('conditions'):
+         fully_qualified_weapon_name = weapon_base_name.get('name') + ' | ' + weapon_skin_name.get('name') + ' (' + weapon_condition + ')'
+         weapon_data_url = 'http://steamcommunity.com/market/priceoverview/?country=US&currency=1&appid=730&market_hash_name=' + fully_qualified_weapon_name
+         r = s.get(weapon_data_url)
+         if(r.json().get('lowest_price') is None):
+            print(fully_qualified_weapon_name + ': request failed.')
+         else:
+            print(fully_qualified_weapon_name + ': ' + r.json().get('lowest_price').replace('&#36;', '$'))
+
+               
